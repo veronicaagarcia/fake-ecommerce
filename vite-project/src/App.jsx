69dc7e66cfc7996ce8a6578339f517de0e3b0c14
Veronica Garcia <veroagarcia90@gmail.com>
2023-08-22 18:26:54 -0300

    Instalación de React con Vite y TailwindCSS

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='bg-slate-800'>
+      Instalación de React con Vite y TailwindCSS
+    </div>
+    
+  )
+}
+
+export default App
